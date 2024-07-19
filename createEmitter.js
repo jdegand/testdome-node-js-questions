3d4fe41991d0dacd97f0f18eba60256d661e4d11
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-07-19 13:36:17 -0400

    fix: remove commented out code in createEmitter.js

--- createEmitter.js
+++ createEmitter.js
@@ -23,35 +23,3 @@ closed(emitter);
 module.exports.createEmitter = createEmitter;
 module.exports.opened = opened;
 module.exports.closed = closed;
-
-/*
-75%
-
-const events = require("events");
-const e = new events();
-
-function createEmitter(onOpen, onClose) {
-    
-  e.once('open', onOpen); 
-  e.once('close', onClose);
-  
-}
-
-function opened(emitter) {
- e.emit('open', emitter); 
-}
-function closed(emitter) {
- e.emit('close', emitter);
-}
-
-let emitter = createEmitter(
-  () => console.log("Opened!"), () => console.log("Closed!")
-);
-opened(emitter);
-closed(emitter);
-
-module.exports.createEmitter = createEmitter;
-module.exports.opened = opened;
-module.exports.closed = closed;
-
-*/
